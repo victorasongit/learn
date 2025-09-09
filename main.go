ee787e4b3490c57820e9a188aeada8001b65a7e8
victorasongit <victor.porcisteanu@gmail.com>
2025-09-09 12:36:46 +0300

    Updated Version

--- main.go
+++ main.go
@@ -5,18 +5,39 @@ import (
 	"math/rand"
 )
 
-var d, n, numb, att int
+var d, n, numb, att, c int
+var s string
 
 func main() {
 	// Initialization of code
+MENU:
+	fmt.Println("\nWelcome to my random number generator guessing game!")
+	fmt.Println("\nThis is the main menu. Please choose your desired option: ")
+	fmt.Println("1. Start the game \n2. Read the rules of the game \n3. Quit the game")
 
-	fmt.Println("Welcome to my random number generator guessing game!\nPlease select your difficulty: ")
-	
+	fmt.Scan(&c)
+	switch {
+	case c == 1:
+		goto BEGINNING
+	case c == 2:
+		fmt.Println("The rules of the game are simple: the computer generates a random number and the user ( i.e. you ) will have to guess it.\nGood luck!")
+		fmt.Println("Would you like to try the game now? Please type Y or N")
+		fmt.Scan(&s)
+		switch {
+		case s == "Y":
+			goto BEGINNING
+		case s == "N":
+			goto MENU
+		}
+	case c == 3:
+		goto QUIT
+	}
+
 	// Choosing difficulty
-
+	s = ""
 BEGINNING:
-	fmt.Println("Difficulty:\n Easy (Press 1)\n Medium (Press 2) \n Hard (Press 3)")
-	fmt.Scan(&d) 
+	fmt.Println("\nDifficulty:\nEasy (Press 1)\nMedium (Press 2) \nHard (Press 3)")
+	fmt.Scan(&d)
 	switch {
 	case d == 1:
 		fmt.Println("You chose the Easy difficulty!")
@@ -33,12 +54,10 @@ BEGINNING:
 	}
 
 	// Main game code
-	
 	fmt.Println("A random number was generated!")
 
 	fmt.Println("Please try to guess the number!")
-
-MAIN_STAGE:
+GUESS_AGAIN:
 	fmt.Scan(&n)
 	if n != numb {
 		fmt.Println("Your guess was incorrect! Please try again!")
@@ -48,7 +67,7 @@ MAIN_STAGE:
 			fmt.Println("The number is larger.")
 		}
 		att++
-		goto MAIN_STAGE
+		goto GUESS_AGAIN
 	} else {
 		fmt.Println("You guessed the number correctly!")
 		att++
@@ -57,6 +76,17 @@ MAIN_STAGE:
 
 	// End celebration
 
-END:	
-	fmt.Println("You won the game! The number of attempts is:", att)	
+END:
+	fmt.Println("You won the game! The number of attempts is:", att)
+	fmt.Println("Would you like to play again? Please type Y or N.")
+	fmt.Scan(&s)
+	switch {
+	case s == "Y":
+		att = 0
+		goto BEGINNING
+	case s == "N":
+		goto QUIT
+	}
+QUIT:
+	fmt.Println("Goodbye!")
 }
